51cbc62bec58281ac3e600164d4ec3ceaf5e45f7
Roy <arindamroy11235@gmail.com>
2025-06-10 17:16:36 +0530

    Integrate custom segmentation model for Dutch language support in audio processing; update handler to utilize language-specific models and adjust test input for new audio source.

--- handler.py
+++ handler.py
@@ -10,6 +10,9 @@ from pyannote.audio import Pipeline
 from runpod.serverless.utils import download_files_from_urls
 from utils import get_pyannote_input_dict, format_diarization_output, join_diarization_output, calculate_amplitude_ratio, detect_cross_talk, calculate_silence_ratio
 import logging
+from diarizers import SegmentationModel
+from pyannote.audio.pipelines import SpeakerDiarization
+from pyannote.pipeline import Optimizer
 
 logger = logging.getLogger(__name__)
 
@@ -19,7 +22,7 @@ assert hf_token, "HF_TOKEN is not set"
 
 def init():
     """Initialize the diarization pipeline."""
-    global pipeline, device    
+    global pipeline, device, segmentation_model_dict    
     if not hf_token:
         raise ValueError("HF_TOKEN environment variable not set")    
     pipeline = Pipeline.from_pretrained(
@@ -29,6 +32,8 @@ def init():
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     pipeline.to(device)
     logger.info(f"Diarization pipeline loaded successfully on {device}")
+
+    segmentation_model_dict={'nl': SegmentationModel().from_pretrained("Roy2358/speaker-segmentation-fine-tuned-nl").to_pyannote_model()}
 
 
 def handler(event):
@@ -47,8 +52,14 @@ def handler(event):
     job_id = event["id"]
     audio_data = job_input.get("audio_data")
     audio_url = job_input.get("audio")
-
+    language = job_input.get("language")
+    
     try:
+        if language in segmentation_model_dict:
+            logger.debug(f"Using segmentation model for {language}")
+            pipeline._segmentation.model = segmentation_model_dict[language].to(device)
+        else:
+            logger.debug(f"Using default segmentation model")
         input_dict = get_pyannote_input_dict(audio_url, audio_data)
         waveform, sample_rate = input_dict["waveform"], input_dict["sample_rate"]
         diarization, embeddings = pipeline(input_dict, return_embeddings=True) 
@@ -62,8 +73,8 @@ def handler(event):
             "noise_ratio": noise_ratio,
             "cross_talk_instances": cross_talk_instances,
             "silence_ratio": silence_ratio,
-            # "embeddings": embeddings,
             "joined_diarization": joined_diarization,
+            # "embeddings": embeddings,
         }
     except Exception as e:
         return {"error": f"Error processing audio: {str(e)}"}
--- test.py
+++ test.py
@@ -1,5 +1,8 @@
+from diarizers import SegmentationModel
 import requests
 from pyannote.audio import Pipeline
+from pyannote.audio.pipelines import SpeakerDiarization
+from pyannote.pipeline import Optimizer
 import os
 from dotenv import load_dotenv
 import torch
@@ -12,26 +15,31 @@ import pickle
 from utils import *
 import io
 
+load_dotenv()
+
+device = torch.device("cuda:0") if torch.cuda.is_available() else torch.device("cpu")
+
 HF_TOKEN = os.getenv("HF_TOKEN")
 assert HF_TOKEN, "HF_TOKEN is not set"
 
 logger = logging.getLogger(__name__)
 # audio_url = "https://github.com/runpod-workers/sample-inputs/raw/refs/heads/main/audio/Arthur.mp3"
-audio_url ='https://github.com/Dembrane/diarization_runpod/raw/refs/heads/feature/echo-262-runpod-serverless-changes-to-accommodate-this/test.mp3'
+audio_url ='https://github.com/Dembrane/diarization_runpod/raw/refs/heads/main/test_dutch.mp3'
 
 input_dict = get_pyannote_input_dict(audio_url, None)
 waveform, sample_rate = input_dict["waveform"], input_dict["sample_rate"]
 
-pipeline = Pipeline.from_pretrained(
-    "pyannote/speaker-diarization-3.1",
-    use_auth_token=HF_TOKEN,
-    )
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+# Load your custom segmentation model
+
+# Create custom pipeline with your segmentation model
+pipeline = Pipeline.from_pretrained("pyannote/speaker-diarization-3.1")
 pipeline.to(device)
+model = SegmentationModel().from_pretrained("Roy2358/speaker-segmentation-fine-tuned-nl")
+model = model.to_pyannote_model()
 
+# pipeline._segmentation.model = model.to(device)
 
-# input_dict = get_pyannote_input_dict(audio_path, None)
-
+# Run diarization
 diarization, embeddings = pipeline(input_dict, return_embeddings=True)
 
 labels = diarization.labels()
